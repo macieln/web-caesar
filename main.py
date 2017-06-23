4d2201167f6d4667a7a4bfadb18066b19b34d4e8
Nahum Maciel <nahumamaciel@gmail.com>
2017-06-22 17:39:07 -0700

    Used remote on Odin pc.

--- main.py
+++ main.py
@@ -3,33 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-form = 
-"""
+
+form = """
     <!DOCTYPE html>
-            <hmtl>
-                <head>
-                    <style>
-                        form {
-                            background-color: #eee;
-                            padding: 20px;
-                            margin: 0 auto;
-                            width: 540px;
-                            font: 16px sans-serif;
-                            border-radius: 10px; 
-                        }
-                         textarea {
-                            margin: 10px 0;
-                            width: 540px;
-                            height: 120px;
-                        }
-                    </style>
-                </head>
-                <body>
-                <!-- create your form here -->
-                </body>
-            </html>               
-"""
+        <html>
+            <head>
+                <style>
+                    form {
+                        background-color: #eee;
+                        padding: 20px;
+                        margin: 0 auto;
+                        width: 540px;
+                        font: 16px sans-serif;
+                        border-radius: 10px
+                    }
+                    textarea {
+                        margin: 10px 0;
+                        width: 540px;
+                        height: 120px;
+                    }
+                </style>
+            </head>
+            <body>
+                <!-- Create your form -->
+            </body>
+        </html>
+        """
+@app.route("/")
 def index():
     return 'Hello World!'
 
